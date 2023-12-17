74f2e80fc3c221829e49a805751217161f3df472
Ananya Garg <ananya.13garg@gmail.com>
2023-12-17 23:09:34 +0530

    Add dashboard

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "gofr.dev/pkg/gofr"
+
+func main() {
+    
+    app := gofr.New()
+
+    
+    app.GET("/dashboard", func(ctx *gofr.Context) (interface{}, error) {
+
+			return "Hello students! Welcome to the placement dashboard", nil
+		})
+
+    // Starts the server, it will listen on the default port 8000.
+    // it can be over-ridden through configs
+    app.Start()
+}
