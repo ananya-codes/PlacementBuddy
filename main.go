4d1e3da5c213ae065b31c1fc8bafab104b93eb46
Ananya Garg <ananya.13garg@gmail.com>
2023-12-17 23:27:02 +0530

    env and sqlDB set

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "gofr.dev/pkg/gofr"
 
+type Company struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
 func main() {
     
     app := gofr.New()
@@ -11,6 +16,8 @@ func main() {
 
 			return "Hello students! Welcome to the placement dashboard", nil
 		})
+
+		
 
     // Starts the server, it will listen on the default port 8000.
     // it can be over-ridden through configs
