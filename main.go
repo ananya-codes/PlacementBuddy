1fba8586ade350eb01d5bd9feff037bdde4fd1a7
Ananya Garg <ananya.13garg@gmail.com>
2023-12-18 00:29:04 +0530

    added package & delete option

--- main.go
+++ main.go
@@ -3,23 +3,66 @@ package main
 import "gofr.dev/pkg/gofr"
 
 type Company struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID      int    `json:"id"`
+	Name    string `json:"name"`
+	Package int    `json:"package"`
 }
 
 func main() {
-    
-    app := gofr.New()
 
-    
-    app.GET("/dashboard", func(ctx *gofr.Context) (interface{}, error) {
+	app := gofr.New()
 
-			return "Hello students! Welcome to the placement dashboard", nil
-		})
+	app.GET("/dashboard", func(ctx *gofr.Context) (interface{}, error) {
 
-		
+		return "Hello students! Welcome to the placement dashboard", nil
+	})
+	
+	app.DELETE("/company/{id}", func(ctx *gofr.Context) (interface{}, error){
+		id := ctx.PathParam("id")
 
-    // Starts the server, it will listen on the default port 8000.
-    // it can be over-ridden through configs
-    app.Start()
+		_, err := ctx.DB().ExecContext(ctx, "DELETE FROM companies WHERE id = (?);", id)
+
+		return nil, err
+	})
+
+	app.POST("/company/{name}/{package}", func(ctx *gofr.Context) (interface{}, error) {
+		name := ctx.PathParam("name")
+		packagee := ctx.PathParam("package")
+
+		_, err := ctx.DB().ExecContext(ctx, "INSERT INTO companies (name, package) VALUES (?, ?)", name, packagee)
+
+		return nil, err
+	})
+
+	app.POST("/company/{name}", func(ctx *gofr.Context) (interface{}, error) {
+		name := ctx.PathParam("name")
+
+		_, err := ctx.DB().ExecContext(ctx, "INSERT INTO companies (name) VALUES (?)", name)
+
+		return nil, err
+	})
+
+	app.GET("/company", func(ctx *gofr.Context) (interface{}, error) {
+		var companies []Company
+
+		rows, err := ctx.DB().QueryContext(ctx, "SELECT * FROM companies;")
+		if err != nil {
+			return nil, err
+		}
+
+		for rows.Next() {
+			var company Company
+			if err := rows.Scan(&company.ID, &company.Name, &company.Package); err != nil {
+				return nil, err
+			}
+
+			companies = append(companies, company)
+		}
+
+		return companies, nil
+	})
+	
+
+	
+	app.Start()
 }
